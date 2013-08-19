fb55beaa7e33c5a31413f77d8e8b1ab6d625ac60
Eric Lamb <eric@mithra62.com>
2013-08-18 18:12:54 -0600

    Initial install of ZF Skeleton and Moji base Module structure NOTE! Must run `composer install` once cloned!!!

---
+++ module/Api/config/module.config.php
@@ -0,0 +1,34 @@
+<?php
+return array(
+	'controllers' => array(
+			'invokables' => array(
+					'Api\Controller\Index' => 'Api\Controller\IndexController',
+			),
+	),
+		
+	'router' => array(
+			'routes' => array(
+					'api' => array(
+							'type'    => 'segment',
+							'options' => array(
+									'route'    => '/api[/][:action][/:id]',
+									'constraints' => array(
+											'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+											'id'     => '[0-9]+',
+									),
+									'defaults' => array(
+											'controller' => 'Api\Controller\Index',
+											'action'     => 'index',
+									),
+							),
+					),
+			),
+	),
+		
+		
+	'view_manager' => array(
+			'template_path_stack' => array(
+					'album' => __DIR__ . '/../view',
+			),
+	),		
+);
