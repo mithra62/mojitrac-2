925eb40a8cd79586c7c9949a5d05cb9666b9dda2
Eric Lamb <eric@mithra62.com>
2014-05-24 04:28:42 -0700

    Removes zftool.phar Adds base docblocks to Module object

--- module/HostManager/Module.php
+++ module/HostManager/Module.php
@@ -1,6 +1,26 @@
 <?php
+ /**
+ * mithra62 - MojiTrac
+ *
+ * @package		mithra62:Mojitrac
+ * @author		Eric Lamb
+ * @copyright	Copyright (c) 2014, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		2.0
+ * @filesource 	./module/HostManager/Module.php
+ */
+
 namespace HostManager;
 
+use Zend\ModuleManager\ModuleManager;
+
+/**
+ * HostManager - Module Object
+ *
+ * @package 	mithra62:Mojitrac
+ * @author		Eric Lamb
+ * @filesource 	./module/HostManager/Module.php
+ */
 class Module
 {
     public function getConfig()
