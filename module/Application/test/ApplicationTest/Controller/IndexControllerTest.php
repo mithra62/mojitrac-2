7f75ef0d65a8159d287bc660ecf029e3be175639
Eric Lamb <eric@mithra62.com>
2014-08-23 23:51:31 -0700

    Renamed earlier Index Test controller

--- module/Application/test/ApplicationTest/Controller/IndexControllerTest.php
+++ module/Application/test/ApplicationTest/Controller/IndexControllerTest.php
@@ -1,10 +1,20 @@
 <?php
+ /**
+ * mithra62 - MojiTrac
+ *
+ * @package		mithra62:Mojitrac
+ * @author		Eric Lamb
+ * @copyright	Copyright (c) 2014, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		2.0
+ * @filesource 	./module/Application/test/ApplicationTest/Controller/ApplicationControllerTest.php
+ */
 
 namespace ApplicationTest\Controller;
 
 use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 
-class ApplicationControllerTest extends AbstractHttpControllerTestCase
+class IndexControllerTest extends AbstractHttpControllerTestCase
 {
 	protected $traceError = true;
 	
@@ -23,6 +33,7 @@ class ApplicationControllerTest extends AbstractHttpControllerTestCase
     	$this->assertModuleName('Application');
     	$this->assertControllerName('Application\Controller\Index');
     	$this->assertControllerClass('IndexController');
+    	$this->assertActionName('index');
     	$this->assertMatchedRouteName('home');
-    }    
+    }   
 }
